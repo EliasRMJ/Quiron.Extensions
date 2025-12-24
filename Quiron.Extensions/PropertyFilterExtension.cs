ad72242f0a0c475b1785e20837bdf5973ea16856
Elias MJunior <34381049+EliasRMJ@users.noreply.github.com>
2025-12-24 19:31:31 -0300

    An improvement was made to property filtering in aligned classes.

--- Quiron.Extensions/PropertyFilterExtension.cs
+++ Quiron.Extensions/PropertyFilterExtension.cs
@@ -4,41 +4,103 @@ namespace Quiron.Extensions
 {
     public static class PropertyFilterExtension
     {
-        public static IEnumerable<IDictionary<string, object>> ToFilterString<T>(this IEnumerable<T> list, string? fields = "")
+        public static IEnumerable<IDictionary<string, object>> ToFilterString<T>(this IEnumerable<T> list
+            , string? fields = "")
         {
             if (list is null || !list.Any())
                 return [];
 
-            var fieldList = fields?.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
-                           .Select(prop => prop.ToLower())
-                           .ToHashSet();
+            var fieldTree = BuildFieldTree(fields);
 
             return list.Select(obj =>
             {
                 var expando = new ExpandoObject() as IDictionary<string, object>;
-
-                var props = typeof(T).GetProperties();
-
-                foreach (var prop in props)
-                {
-                    if (fieldList is null || fieldList.Count == 0 || fieldList.Contains(prop.Name.ToLower()))
-                    {
-                        var value = prop.GetValue(obj);
-                        if (value is not null)
-                        {
-                            var camelCaseName = char.ToLower(prop.Name[0]) + prop.Name[1..];
-                            expando[camelCaseName] = value;
-                        }
-                    }
-                }
+                FillObject(expando, obj!, fieldTree);
 
                 return expando;
             });
         }
 
-        public static IEnumerable<IDictionary<string, object>> ToFilterString<T>(this List<T> list, string? fields = "")
+        public static IEnumerable<IDictionary<string, object>> ToFilterString<T>(this List<T> list
+            , string? fields = "")
         {
             return ToFilterString<T>(list.AsEnumerable(), fields);
+        }
+
+        private static Dictionary<string, object?> BuildFieldTree(string? fields)
+        {
+            var tree = new Dictionary<string, object?>();
+
+            if (string.IsNullOrWhiteSpace(fields))
+                return tree;
+
+            foreach (var field in fields.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
+            {
+                var parts = field.Split('.');
+                var current = tree;
+
+                foreach (var part in parts)
+                {
+                    var key = part.ToLower();
+                    if (!current.ContainsKey(key))
+                        current[key] = new Dictionary<string, object?>();
+
+                    current = (Dictionary<string, object?>)current[key]!;
+                }
+            }
+
+            return tree;
+        }
+
+        private static void FillObject(IDictionary<string, object> target, object source
+            , Dictionary<string, object?> fieldTree)
+        {
+            var props = source.GetType().GetProperties();
+
+            foreach (var prop in props)
+            {
+                var propKey = prop.Name.ToLower();
+
+                if (fieldTree.Count > 0 && !fieldTree.ContainsKey(propKey))
+                    continue;
+
+                var value = prop.GetValue(source);
+                if (value is null)
+                    continue;
+
+                var camelCaseName = char.ToLower(prop.Name[0]) + prop.Name[1..];
+                var subTree = fieldTree.TryGetValue(propKey, out object? value1)
+                    ? value1 as Dictionary<string, object?>
+                    : null;
+
+                if (subTree is null || subTree.Count.Equals(0) || IsSimpleType(prop.PropertyType))
+                {
+                    target[camelCaseName] = value;
+                }
+                else
+                {
+                    var nested = new ExpandoObject() as IDictionary<string, object>;
+                    
+                    FillObject(nested!, value, subTree);
+                    target[camelCaseName] = nested!;
+                }
+            }
+        }
+
+        private static bool IsSimpleType(Type type)
+        {
+            type = Nullable.GetUnderlyingType(type) ?? type;
+
+            return type.IsPrimitive
+                || type.IsEnum
+                || type == typeof(int)
+                || type == typeof(long)
+                || type == typeof(string)
+                || type == typeof(DateTime)
+                || type == typeof(DateOnly)
+                || type == typeof(decimal)
+                || type == typeof(double)
+                || type == typeof(Guid);
         }
     }
 }
