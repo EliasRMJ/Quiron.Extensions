76473a07874457c40de8e5420f33c0edef3f0d4d
Elias MJunior <34381049+EliasRMJ@users.noreply.github.com>
2026-01-27 14:07:19 -0300

    Add CultureInfo into GetDescription method

--- Quiron.Extensions/EnumExtension.cs
+++ Quiron.Extensions/EnumExtension.cs
@@ -1,15 +1,19 @@
-﻿namespace Quiron.Extensions
+﻿using System.Globalization;
+
+namespace Quiron.Extensions
 {
     [AttributeUsage(AttributeTargets.All)]
-    public class DescriptionEnumAttribute(string description)
+    public class DescriptionEnumAttribute(string defaultValue, string pt = "", string es = "")
         : Attribute
     {
-        public string Description { get; set; } = description;
+        public string Description { get; set; } = defaultValue;
+        public string Pt { get; } = pt;
+        public string Es { get; } = es;
     }
 
     public static class EnumExtension
     {
-        public static string? GetDescription(this Enum value)
+        public static string? GetDescription(this Enum value, CultureInfo? culture = null)
         {
             try
             {
@@ -19,7 +23,17 @@
                 if (info != null)
                 {
                     var atributos = info.GetCustomAttributes(typeof(DescriptionEnumAttribute), false) as DescriptionEnumAttribute[];
-                    return atributos!.Length == 0 ? null : atributos[0].Description;
+                    var name = atributos!.Length > 0 ? atributos[0].Description : null;
+
+                    if (culture != null)
+                    {
+                        if (culture.Name == "pt" || culture.Name == "pt-BR")
+                            name = atributos[0].Pt;
+                        else if (culture.Name == "es" || culture.Name == "es-ES")
+                            name = atributos[0].Es;
+                    }
+
+                    return name;
                 }
                 else { return string.Empty; }
             }
